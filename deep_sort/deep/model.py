2887c670838312564cccd9d0c9f018af9cc259a7
yeokwangeun <yeokwangeun@gmail.com>
2023-06-07 22:00:07 +0900

    CNN model

--- deep_sort/deep/model.py
+++ deep_sort/deep/model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import einops
+
 
 class BasicBlock(nn.Module):
     def __init__(self, c_in, c_out,is_downsample=False):
@@ -46,9 +48,10 @@ def make_layers(c_in,c_out,repeat_times, is_downsample=False):
     return nn.Sequential(*blocks)
 
 class Net(nn.Module):
-    def __init__(self, num_classes=751 ,reid=False):
+    def __init__(self, num_classes=315 ,reid=False, low_fusion=None):
         super(Net,self).__init__()
         # 3 128 64
+        self.low_fusion = low_fusion
         self.conv = nn.Sequential(
             nn.Conv2d(3,64,3,stride=1,padding=1),
             nn.BatchNorm2d(64),
@@ -59,11 +62,20 @@ class Net(nn.Module):
             nn.MaxPool2d(3,2,padding=1),
         )
         # 32 64 32
+        get_out_layer = lambda in_channel: nn.Sequential(
+            nn.Conv2d(in_channel, 512, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((8, 4))
+        )
         self.layer1 = make_layers(64,64,2,False)
+        self.out1 = get_out_layer(64)
         # 32 64 32
         self.layer2 = make_layers(64,128,2,True)
+        self.out2 = get_out_layer(128)
         # 64 32 16
         self.layer3 = make_layers(128,256,2,True)
+        self.out3 = get_out_layer(256)
         # 128 16 8
         self.layer4 = make_layers(256,512,2,True)
         # 256 8 4
@@ -77,13 +89,34 @@ class Net(nn.Module):
             nn.Dropout(),
             nn.Linear(256, num_classes),
         )
+        self.concat_fusion_conv = nn.Conv2d(512*4, 512, kernel_size=1, stride=1, bias=False)
+        self.gate_conv = nn.Conv3d(512, 1, kernel_size=(1, 1, 1))
     
     def forward(self, x):
         x = self.conv(x)
         x = self.layer1(x)
+        x1 = self.out1(x)
         x = self.layer2(x)
+        x2 = self.out2(x)
         x = self.layer3(x)
+        x3 = self.out3(x)
         x = self.layer4(x)
+        x4 = x
+
+        if self.low_fusion == "add":
+            x = x1 + x2 + x3 + x4
+        elif self.low_fusion == "concat":
+            x = torch.cat([x1, x2, x3, x4], axis=1)
+            x = self.concat_fusion_conv(x)
+        elif self.low_fusion == "gate":
+            stacked = torch.stack([x1, x2, x3, x4], axis=2)
+            gate = self.gate_conv(stacked)
+            gate = einops.repeat(gate.squeeze(1), "b n h w -> b d n h w", d=512)
+            x = stacked * gate
+            x = torch.sum(x, axis=2)
+        else:
+            pass
+        
         x = self.avgpool(x)
         x = x.view(x.size(0),-1)
         # B x 128
